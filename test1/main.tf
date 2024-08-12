c8f3d203be1197a24d088cdfac7ccb129c59e901
tranvannhan1911 <71268800+tranvannhan1911@users.noreply.github.com>
2024-08-12 21:04:26 +0700

    test1

---
+++ test1/main.tf
@@ -0,0 +1,17 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.53.0"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+}
+
+output "print_password" {
+  value = var.password
+}
