4b1c7caeca6009cc2501832513a8193445b75dfa
Linux User <tranvannhan1911@gmail.com>
2024-08-12 21:44:07 +0000

    Add main.tf and variables.tf in cloudify-1723494986874 directory

---
+++ cloudify-1723494986874/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+resource "openstack_compute_instance_v2" "openstack_compute_instance_v2_1" {
+  name              = "openstack_compute_instance_v2_1"
+  count             = 1
+  image_id          = ""
+  key_name          = ""
+  availability_zone = ""
+  flavor_id         = ""
+  user_data         = ""
+}
+
