e414e622605354de921a1cda30b0212240c4cf21
Linux User <tranvannhan1911@gmail.com>
2024-08-13 00:42:26 +0000

    Add main.tf and variables.tf in cloudify-1723494986874 directory

--- cloudify-1723494986874/main.tf
+++ cloudify-1723494986874/main.tf
@@ -2,63 +2,13 @@ terraform {
   required_version = ">= 0.12"
 }
 
-resource "openstack_compute_instance_v2" "Server_20240813T003444504Z" {
-  name              = "Server_20240813T003444504Z"
+resource "openstack_compute_instance_v2" "Server_20240813T004224175Z" {
+  name              = "Server_20240813T004224175Z"
   count             = 1
   image_id          = ""
   key_name          = ""
   availability_zone = ""
   flavor_id         = ""
   user_data         = ""
-}
-
-resource "openstack_lb_loadbalancer_v2" "Load_Balancer_20240813T003445703Z" {
-  name        = "Load_Balancer_20240813T003445703Z"
-  description = ""
-}
-
-resource "openstack_compute_instance_v2" "Server_20240813T003450368Z" {
-  name              = "Server_20240813T003450368Z"
-  count             = 1
-  image_id          = ""
-  key_name          = ""
-  availability_zone = ""
-  flavor_id         = ""
-  user_data         = ""
-}
-
-resource "openstack_compute_instance_v2" "Server_20240813T003658199Z" {
-  name              = "Server_20240813T003658199Z"
-  count             = 1
-  image_id          = ""
-  key_name          = ""
-  availability_zone = ""
-  flavor_id         = ""
-  user_data         = ""
-}
-
-resource "openstack_compute_instance_v2" "Server_20240813T004117849Z" {
-  name              = "Server_20240813T004117849Z"
-  count             = 1
-  image_id          = ""
-  key_name          = ""
-  availability_zone = ""
-  flavor_id         = ""
-  user_data         = ""
-}
-
-resource "openstack_compute_instance_v2" "Server_20240813T004140223Z" {
-  name              = "Server_20240813T004140223Z"
-  count             = 1
-  image_id          = ""
-  key_name          = ""
-  availability_zone = ""
-  flavor_id         = ""
-  user_data         = ""
-}
-
-resource "openstack_lb_loadbalancer_v2" "Load_Balancer_20240813T004201854Z" {
-  name        = "Load_Balancer_20240813T004201854Z"
-  description = ""
 }
 
