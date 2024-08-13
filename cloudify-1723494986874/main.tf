8931be7e60ce8dd31b66d7450a67d9dbbd05ca59
Linux User <tranvannhan1911@gmail.com>
2024-08-13 00:22:15 +0000

    Add main.tf and variables.tf in cloudify-1723494986874 directory

--- cloudify-1723494986874/main.tf
+++ cloudify-1723494986874/main.tf
@@ -2,8 +2,23 @@ terraform {
   required_version = ">= 0.12"
 }
 
-resource "openstack_compute_instance_v2" "Server_20240813T001838132Z" {
-  name              = "Server_20240813T001838132Z"
+resource "openstack_compute_instance_v2" "Server_20240813T002021100Z" {
+  name              = "Server_20240813T002021100Z"
+  count             = 1
+  image_id          = ""
+  key_name          = ""
+  availability_zone = ""
+  flavor_id         = ""
+  user_data         = ""
+}
+
+resource "openstack_lb_loadbalancer_v2" "Load_Balancer_20240813T002031523Z" {
+  name        = "Load_Balancer_20240813T002031523Z"
+  description = ""
+}
+
+resource "openstack_compute_instance_v2" "Server_20240813T002047122Z" {
+  name              = "Server_20240813T002047122Z"
   count             = 1
   image_id          = ""
   key_name          = ""
