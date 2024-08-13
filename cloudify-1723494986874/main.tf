c6895a2a4cb243a75bc8275a5dd76c5caad1fc1a
Linux User <tranvannhan1911@gmail.com>
2024-08-13 05:40:03 +0000

    Add main.tf and variables.tf in cloudify-1723494986874 directory

--- cloudify-1723494986874/main.tf
+++ cloudify-1723494986874/main.tf
@@ -8,7 +8,7 @@ resource "openstack_compute_instance_v2" "openstack_compute_instance_v2_1" {
   image_id          = "b49a9f66-d9a8-4e55-a3c7-772dead91986"
   key_name          = "demo-key"
   availability_zone = ""
-  flavor_id         = ""
+  flavor_id         = "Small-1"
   user_data         = ""
 }
 
